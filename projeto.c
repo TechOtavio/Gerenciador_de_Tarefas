8bcb6a9c1f8b4929dc8976a0b85586a86833d378
SamuelEngDev <samuelaugustobessa@gmail.com>
2026-09-01 18:40:21 -0300

    Samuel-Add_tarefa

--- projeto.c
+++ projeto.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct Tarefa{
+
+    int Id;
+    char *Nome;
+    char *Descricao;
+    char *Status;
+
+    struct Tarefa *prox;
+}Tarefa;
+
+
+Tarefa *inicio = NULL;
+int tam = 0;
+
+void add_Tarefa(int id , char *nome , char *descricao , char *status , int pos){
+    Tarefa *novo = malloc(sizeof(Tarefa));
+
+    novo->Id = id;
+    novo->Nome = nome;
+    novo->Descricao = descricao;
+    novo->Status = status;
+    novo->prox = NULL;
+
+    //Conectar os Nós;
+
+    if(inicio == NULL){
+        inicio = novo;
+        tam++;
+    } else {
+        if(pos == 0){
+            novo->prox = inicio;
+            inicio = novo;
+            tam ++;
+        } else if(pos == tam){
+            Tarefa *aux = inicio;
+
+            for(int i = 0; i < tam -1 ; i++){
+                aux = aux->prox;
+            }
+            aux->prox = novo;
+            tam++;
+        }
+    }
+}
+
+
+void ler_tarefas(){
+    
+}
+
+    
 int main(){
-    printf("TEste");
-    return 0;
+
+
 }
